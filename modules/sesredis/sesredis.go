7332e36cc7031b66ff1e41cc0e11b955b93800ae
qiang <guyan0319@gmail.com>
2019-10-28 18:30:04 +0800

    redis

--- modules/sesredis/sesredis.go
+++ modules/sesredis/sesredis.go
@@ -3,13 +3,47 @@ package sesredis
 import (
 	"container/list"
 	"sync"
+	"time"
 )
 
 //session来自redis 实现
 type FromRedis struct {
 	lock     sync.Mutex               //用来锁
 	sessions map[string]*list.Element //用来存储在内存
-	list     *list.List               //用来做 gc
 }
 
 
+//session实现
+type SessionStore struct {
+	sid              string                      //session id 唯一标示
+	LastAccessedTime time.Time                   //最后访问时间
+	value            map[interface{}]interface{} //session 里面存储的值
+}
+
+//设置
+func (st *SessionStore) Set(key, value interface{}) error {
+	st.value[key] = value
+	pder.SessionUpdate(st.sid)
+	return nil
+}
+
+//获取session
+func (st *SessionStore) Get(key interface{}) interface{} {
+	pder.SessionUpdate(st.sid)
+	if v, ok := st.value[key]; ok {
+		return v
+	} else {
+		return nil
+	}
+	return nil
+}
+
+//删除
+func (st *SessionStore) Delete(key interface{}) error {
+	delete(st.value, key)
+	pder.SessionUpdate(st.sid)
+	return nil
+}
+func (st *SessionStore) SessionID() string {
+	return st.sid
+}
