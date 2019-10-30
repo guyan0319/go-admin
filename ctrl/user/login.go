264427698ab076d46c54ba0cdb5e21b3166f9a84
qiang <guyan0319@gmail.com>
2019-10-30 18:32:11 +0800

    cors

--- ctrl/user/login.go
+++ ctrl/user/login.go
@@ -1,44 +1,49 @@
 package user
 
 import (
-	"github.com/gin-contrib/sessions"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"go-admin/models"
-	"go-admin/modules/response"
-	"go-admin/public/common"
-	"strconv"
-	"time"
 )
+type User struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password   string `form:"password" json:"password" binding:"required"`
+}
 func Login(c *gin.Context) {
-	nickname := c.PostForm("nickname")
-	passwd := c.PostForm("passwd")
-	if nickname == "" || passwd == "" {
-		response.ShowError(c, "fail")
-		return
-	}
-	user := models.SystemUser{Nickname: nickname}
-	has := user.GetRowByNickname()
-	if !has {
-		response.ShowError(c, "fail")
-		return
-	}
-	if common.Sha1En(passwd+user.Salt) != user.Password {
-		response.ShowError(c, "fail")
-		return
-	}
-	session := sessions.Default(c)
-	var data = make(map[string]interface{}, 0)
-	v := session.Get("token")
-	if v == nil {
-		cur := time.Now()
-		//纳秒
-		timestamps := cur.UnixNano()
-		times := strconv.FormatInt(timestamps, 10)
-		v = common.Md5En(common.GetRandomString(16) + times)
-		session.Set("token", v)
-		_=session.Save()
-	}
-	data["token"] = v
-	response.ShowData(c, data)
+	var u User
+	err :=c.BindJSON(&u)
+	fmt.Println(err)
+	fmt.Println(u)
+	//nickname := c.PostForm("username")
+	//passwd := c.PostForm("password")
+	//param3,_ :=c.GetPostForm("username")
+	//fmt.Println(param3)
+	//if nickname == "" || passwd == "" {
+	//	response.ShowError(c, "fail")
+	//	return
+	//}
+	//user := models.SystemUser{Nickname: nickname}
+	//has := user.GetRowByNickname()
+	//if !has {
+	//	response.ShowError(c, "fail")
+	//	return
+	//}
+	//if common.Sha1En(passwd+user.Salt) != user.Password {
+	//	response.ShowError(c, "fail")
+	//	return
+	//}
+	////session := sessions.Default(c)
+	//var data = make(map[string]interface{}, 0)
+	//v := session.Get("token")
+	//if v == nil {
+	//	cur := time.Now()
+	//	//纳秒
+	//	timestamps := cur.UnixNano()
+	//	times := strconv.FormatInt(timestamps, 10)
+	//	v = common.Md5En(common.GetRandomString(16) + times)
+	//	session.Set("token", v)
+	//	_=session.Save()
+	//}
+	//data["token"] = v
+	//response.ShowData(c, data)
 	return
 }
--- ctrl/user/user.go
+++ ctrl/user/user.go
@@ -3,22 +3,25 @@ package user
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"go-admin/modules/response"
-	"go-admin/public/common"
 )
 
+
 func Reg(c *gin.Context){
-	nickname :=c.Query("nickname")
-	passwd :=c.Query("passwd")
-	fmt.Println(nickname)
-	if nickname=="" || passwd=="" {
-		response.ShowError(c,"fail")
-		return
-	}
-	salt :=common.GetRandomBoth(4)
-	passwd = common.Sha1En(passwd+salt)
-	fmt.Println(salt)
-	fmt.Println(passwd)
+	
+	param3,_ :=c.GetPostForm("username")
+	fmt.Println(c.PostForm("password"))
+	fmt.Println(param3)
+	//nickname :=c.Query("nickname")
+	//passwd :=c.Query("passwd")
+	//fmt.Println(nickname)
+	//if nickname=="" || passwd=="" {
+	//	response.ShowError(c,"fail")
+	//	return
+	//}
+	//salt :=common.GetRandomBoth(4)
+	//passwd = common.Sha1En(passwd+salt)
+	//fmt.Println(salt)
+	//fmt.Println(passwd)
 
 
 }
