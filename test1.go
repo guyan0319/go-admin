ca32529d4ed7b05d7cef95fc7fc50e21ae474b28
qiang <guyan0319@gmail.com>
2019-11-04 18:31:51 +0800

    rest

--- test1.go
+++ test1.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main()  {
+	var data map[string]interface{}
+	jsonStr := "{\"Name\":\"test\",\"Age\":19,\"infos\":[{\"info1\":\"hello\"},{\"info2\":\"hello\"}],\"pic\":{\"pic1\":\"img.jpg\"}}"
+	err := json.Unmarshal([]byte(jsonStr), &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
+	for _,value:=range data{
+		switch value.(type) {
+		case float64:
+			fmt.Println(value)
+		case string:
+			fmt.Println(value)
+		case []interface {}:
+			for k,v:=range value.([]interface {}){
+				fmt.Println(k,v)
+			}
+		case map[string]interface {}:
+			for k,v:=range value.(map[string]interface {}){
+				fmt.Println(k,v)
+			}
+		}
+	}
+
+}
