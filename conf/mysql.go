343b9befd1601c97a0940df4c230366286e53684
admin <guyan0319@163.com>
2021-12-30 17:32:57 +0800

    no message

--- conf/mysql.go
+++ conf/mysql.go
@@ -11,11 +11,7 @@ type DbConfig struct {
 var Db = map[string]DbConfig{
 	"db1": {
 		DriverName: "mysql",
-<<<<<<< HEAD
-		Dsn:        "root:Myyd@159@tcp(127.0.0.1:3306)/systemrdb?charset=utf8mb4&parseTime=true&loc=Local",
-=======
 		Dsn:        "root:123456@tcp(127.0.0.1:3306)/systemdb?charset=utf8mb4&parseTime=true&loc=Local",
->>>>>>> 94b83ae67378b8f339e274bfcb7699ae765af7df
 		ShowSql:    true,
 		ShowExecTime:    false,
 		MaxIdle:    10,
