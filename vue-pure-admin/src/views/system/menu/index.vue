4e45db92f81d96f713de264728d8bf2e95372494
admin <guyan0319@163.com>
2022-04-29 10:25:50 +0800

    no message

--- vue-pure-admin/src/views/system/menu/index.vue
+++ vue-pure-admin/src/views/system/menu/index.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 export default {
-  name: "dept"
+  name: "menu"
 };
 </script>
 
@@ -12,19 +12,156 @@ import { FormInstance } from "element-plus";
 import { reactive, ref, onMounted } from "vue";
 import { EpTableProBar } from "/@/components/ReTable";
 import { useRenderIcon } from "/@/components/ReIcon/src/hooks";
+import {VxeFormPropTypes} from "vxe-table";
 
-const form = reactive({
-  user: "",
-  status: ""
+const dictData = reactive({
+  submitLoading: false,
+  showEdit: false,
+  selectRow: null,
+  filterName: "",
+  formData: {
+    name: "",
+    alias: "",
+    path: "",
+    component: "",
+    redirect: "",
+    url: "",
+    metaTitle: "",
+    metaIcon: "",
+    metaI18n: 0,
+    metaShowlink: 0,
+    metaRank: 0,
+    metaKeepalive: 0,
+    type: 2,
+    metaFramesrc: "",
+    transitionName: "",
+    transitionEnter: "",
+    transitionLeave: "",
+    dynamiclevel: 0,
+    refreshredirect: "",
+    extraiconSvg: 0,
+    extraiconName: "",
+    pid: 0,
+    state: 0,
+    level: "",
+  },
+  formItems: [
+    {
+      field: "name",
+      title: "名称",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: { placeholder: "请输入名称" }
+      }
+    },
+    {
+      field: "alias",
+      title: "别名",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: {
+          placeholder: "请输入别名",
+        }
+      }
+    },
+    {
+      field: "path",
+      title: "路径",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: { placeholder: "请输入路径",}
+      }
+    },
+   {
+      field: "redirect",
+      title: "重定向",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: { placeholder: "请输入重定向",}
+      }
+    },
+ {
+      field: "url",
+      title: "url",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: { placeholder: "请输入url",}
+      }
+    },
+ {
+      field: "metaTitle",
+      title: "meta标题",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: { placeholder: "请输入meta标题",}
+      }
+    },
+ {
+      field: "meta_icon",
+      title: "meta icon",
+      span: 24,
+      itemRender: {
+        name: "$input",
+        props: { placeholder: "请输入meta icon",}
+      }
+    },
+{
+      field: "meta_i18n",
+      title: "是否国际化",
+      span: 24,
+      itemRender: {
+        name: "$radio",
+      }
+    },
+
+    {
+      align: "right",
+      span: 24,
+      itemRender: {
+        name: "$buttons",
+        children: [
+          { props: { type: "submit", content: "提交", status: "primary" } },
+          { props: { type: "reset", content: "重置" } }
+        ]
+      }
+    }
+  ] as VxeFormPropTypes.Items
 });
 let dataList = ref([]);
 let loading = ref(true);
 
 const formRef = ref<FormInstance>();
 const tableRef = ref();
+// 新增
+function onAdd() {
+  commonFn(null, false);
+}
+
+// 新增子类型
+function onAddChild(row?: object) {
+  console.log("onAddChild", row);
+  commonFn(null, false);
+}
 
 function handleUpdate(row) {
+
+  commonFn(row, true);
+  // VXETable.modal.message({
+  //   content: "测试数据，不可编辑",
+  //   status: "error"
+  // });
   console.log(row);
+}
+function commonFn(value, disabled) {
+  dictData.selectRow = value;
+  dictData.showEdit = true;
+  dictData.formItems[1].itemRender.props.disabled = disabled;
 }
 
 function handleDelete(row) {
@@ -60,14 +197,14 @@ onMounted(() => {
     <el-form
       ref="formRef"
       :inline="true"
-      :model="form"
+      :model="dictData.formData"
       class="bg-white w-99/100 pl-8 pt-4"
     >
-      <el-form-item label="部门名称：" prop="user">
-        <el-input v-model="form.user" placeholder="请输入部门名称" clearable />
+      <el-form-item label="菜单名称：" prop="user">
+        <el-input v-model="dictData.formData.name" placeholder="请输入菜单名称" clearable />
       </el-form-item>
-      <el-form-item label="状态：" prop="status">
-        <el-select v-model="form.status" placeholder="请选择状态" clearable>
+      <el-form-item label="状态：" prop="state">
+        <el-select v-model="dictData.formData.state" placeholder="请选择状态" clearable>
           <el-option label="开启" value="1" />
           <el-option label="关闭" value="0" />
         </el-select>
@@ -88,15 +225,15 @@ onMounted(() => {
     </el-form>
 
     <EpTableProBar
-      title="部门列表"
+      title="菜单列表"
       :loading="loading"
       :tableRef="tableRef"
       :dataList="dataList"
       @refresh="onSearch"
     >
       <template #buttons>
-        <el-button type="primary" :icon="useRenderIcon('add')">
-          新增部门
+        <el-button type="primary" :icon="useRenderIcon('add')"  @click="onAdd">
+          新增菜单
         </el-button>
       </template>
       <template v-slot="{ size, checkList }">
@@ -124,16 +261,18 @@ onMounted(() => {
             label="序号"
             width="60"
           />
-          <el-table-column label="部门名称" prop="name" width="180" />
-          <el-table-column label="排序" align="center" prop="sort" width="60" />
-          <el-table-column label="状态" align="center" prop="status" width="80">
+          <el-table-column label="菜单名称" prop="name" width="180" />
+          <el-table-column label="路径" align="center" prop="path" width="160" />
+          <el-table-column label="重定向" align="center" prop="redirect" width="160" />
+          <el-table-column label="标题" align="center" prop="metaTitle" width="160" />
+          <el-table-column label="状态" align="center" prop="state" width="80">
             <template #default="scope">
               <el-tag
                 :size="size"
-                :type="scope.row.status === 0 ? 'danger' : 'success'"
+                :type="scope.row.state === 0 ? 'danger' : 'success'"
                 effect="plain"
               >
-                {{ scope.row.status === 0 ? "关闭" : "开启" }}
+                {{ scope.row.state === 0 ? "关闭" : "开启" }}
               </el-tag>
             </template>
           </el-table-column>
@@ -141,18 +280,12 @@ onMounted(() => {
             label="创建时间"
             align="center"
             width="180"
-            prop="createTime"
+            prop="ctime"
             :formatter="
-              ({ createTime }) => {
-                return dayjs(createTime).format('YYYY-MM-DD HH:mm:ss');
+              ({ ctime }) => {
+                return dayjs(ctime).format('YYYY-MM-DD HH:mm:ss');
               }
             "
-          />
-          <el-table-column
-            label="备注"
-            align="center"
-            prop="remark"
-            show-overflow-tooltip
           />
           <el-table-column
             fixed="right"
@@ -188,5 +321,26 @@ onMounted(() => {
         </el-table>
       </template>
     </EpTableProBar>
+    <!-- 修改、添加弹框 -->
+    <vxe-modal
+      resize
+      width="450"
+      v-model="dictData.showEdit"
+      :title="dictData.selectRow ? '编辑' : '新增'"
+      :loading="dictData.submitLoading"
+      @hide="$refs.xForm.reset()"
+    >
+      <template #default>
+        <vxe-form
+          ref="xForm"
+          :data="dictData.formData"
+          :items="dictData.formItems"
+          title-align="right"
+          title-width="100"
+          @submit="submitEvent"
+        />
+      </template>
+    </vxe-modal>
+
   </div>
 </template>
